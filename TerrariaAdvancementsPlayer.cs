7c4d900be5a90a596d53c4a5d2de0d8f6db6b0cc
Requias <max.orin05@yahoo.com>
2021-03-01 15:16:05 -0500

    TIMBER

--- TerrariaAdvancementsPlayer.cs
+++ TerrariaAdvancementsPlayer.cs
@@ -1,10 +1,13 @@
 ﻿using Terraria.GameInput;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace TerrariaAdvancements
 {
     public class TerrariaAdvancementsPlayer : ModPlayer
     {
+        public bool Timber;
+
         public override void ProcessTriggers(TriggersSet triggersSet)
         {
             if (TerrariaAdvancements.ToggleUIHK.JustPressed)
@@ -17,6 +20,14 @@ namespace TerrariaAdvancements
                 {
                     ModContent.GetInstance<TerrariaAdvancements>().HideUI();
                 }
+            }
+        }
+
+        public override void PreUpdate()
+        {
+            if (player.HasItem(ItemID.Wood) || player.HasItem(ItemID.RichMahogany) || player.HasItem(ItemID.Ebonwood) || player.HasItem(ItemID.Shadewood) || player.HasItem(ItemID.Pearlwood) || player.HasItem(ItemID.BorealWood) || player.HasItem(ItemID.PalmWood))
+            {
+                Timber = true;
             }
         }
     }
--- UI/AdvancementTreeUI.cs
+++ UI/AdvancementTreeUI.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ModLoader;
@@ -7,153 +6,148 @@ using Terraria.UI;
 
 namespace TerrariaAdvancements.UI
 {
-    public class ATHoverImageButton : UIImageButton
-    {
-        internal string HoverText;
-
-        public ATHoverImageButton(Texture2D texture, string hoverText) : base(texture)
-        {
-            HoverText = hoverText;
-        }
-
-        protected override void DrawSelf(SpriteBatch spriteBatch)
-        {
-            base.DrawSelf(spriteBatch);
-
-            if (IsMouseHovering)
-            {
-                Main.hoverItemName = HoverText;
-            }
-        }
-    }
-
-    public class ATHoverImage : UIImage
-    {
-        internal string HoverText;
-
-        public ATHoverImage(Texture2D texture, string hoverText) : base(texture)
-        {
-            HoverText = hoverText;
-        }
-
-        protected override void DrawSelf(SpriteBatch spriteBatch)
-        {
-            base.DrawSelf(spriteBatch);
-
-            if (IsMouseHovering)
-            {
-                Main.hoverItemName = HoverText;
-            }
-        }
-    }
-
     public class AdvancementTreeUI : UIState
     {
-        public ATHoverImageButton HoverImageButton;
+        UIPanel ATPanel = new UIPanel();
 
-        ATHoverImage EyeOnYou_Achievement;
+        UIText HoverText = new UIText("");
 
-        UIPanel SlayerCategoryPanel;
+        UIImageButton TerrariaCategoryButton = new UIImageButton(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/TerrariaCategory"));
+        UIImageButton SlayerCategoryButton = new UIImageButton(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/SlayerCategory"));
+        UIImageButton CollectorCategoryButton = new UIImageButton(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/CollectorCategory"));
+        UIImageButton ExplorerCategoryButton = new UIImageButton(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/ExplorerCategory"));
+        UIImageButton ChallengerCategoryButton = new UIImageButton(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/ChallengerCategory"));
 
-        Texture2D EyeOnYou_LockedTexture = ModContent.GetTexture("TerrariaAdvancements/UI/Images/Achievements/Locked/Slayer/EyeOnYou_Locked");
-        Texture2D EyeOnYou_UnlockedTexture = ModContent.GetTexture("TerrariaAdvancements/UI/Images/Achievements/Unlocked/Slayer/EyeOnYou_Unlocked");
+        UIPanel TerrariaCategoryPanel = new UIPanel();
+        FixedUIScrollbar TerrariaCategoryScrollbar = new FixedUIScrollbar(ModContent.GetInstance<TerrariaAdvancements>().ATInterface);
+
+        UIImage TimberAchievement = new UIImage(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Achievements/Locked/Terraria/Timber_Locked"));
+        UIPanel Timber_HammerTime = new UIPanel();
+        UIPanel Timber_NoHobo = new UIPanel();
 
         public override void OnInitialize()
         {
-            UIPanel ATPanel = new UIPanel();
             ATPanel.Width.Set(500, 0);
             ATPanel.Height.Set(300, 0);
             ATPanel.HAlign = ATPanel.VAlign = 0.5f;
-            Append(ATPanel);
 
-            Texture2D TerrariaCategoryTexture = ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/TerrariaCategory");
-            ATHoverImageButton TerrariaCategoryButton = new ATHoverImageButton(TerrariaCategoryTexture, "Terraria Achievements");
             TerrariaCategoryButton.Width.Set(32, 0);
             TerrariaCategoryButton.Height.Set(32, 0);
             TerrariaCategoryButton.HAlign = 0.05f;
             TerrariaCategoryButton.VAlign = 0.005f;
-            // ChallengerCategoryButton.OnClick += new MouseEvent(ChallengerCategoryButtonClicked);
-            ATPanel.Append(TerrariaCategoryButton);
+            // TerrariaCategoryButton.OnClick += new MouseEvent(ChallengerCategoryButtonClicked);
 
-            Texture2D SlayerCategoryTexture = ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/SlayerCategory");
-            ATHoverImageButton SlayerCategoryButton = new ATHoverImageButton(SlayerCategoryTexture, "Slayer Achievements");
             SlayerCategoryButton.Width.Set(32, 0);
             SlayerCategoryButton.Height.Set(32, 0);
             SlayerCategoryButton.HAlign = 0.15f;
             SlayerCategoryButton.VAlign = 0.005f;
             // SlayerCategoryButton.OnClick += new MouseEvent(SlayerCategoryButtonClicked);
-            ATPanel.Append(SlayerCategoryButton);
 
-            Texture2D CollectorCategoryTexture = ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/CollectorCategory");
-            ATHoverImageButton CollectorCategoryButton = new ATHoverImageButton(CollectorCategoryTexture, "Collector Achievements");
             CollectorCategoryButton.Width.Set(32, 0);
             CollectorCategoryButton.Height.Set(32, 0);
             CollectorCategoryButton.HAlign = 0.25f;
             CollectorCategoryButton.VAlign = 0.005f;
             // CollectorCategoryButton.OnClick += new MouseEvent(CollectorCategoryButtonClicked);
-            ATPanel.Append(CollectorCategoryButton);
 
-            Texture2D ExplorerCategoryTexture = ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/ExplorerCategory");
-            ATHoverImageButton ExplorerCategoryButton = new ATHoverImageButton(ExplorerCategoryTexture, "Explorer Achievements");
             ExplorerCategoryButton.Width.Set(32, 0);
             ExplorerCategoryButton.Height.Set(32, 0);
             ExplorerCategoryButton.HAlign = 0.35f;
             ExplorerCategoryButton.VAlign = 0.005f;
             // ExplorerCategoryButton.OnClick += new MouseEvent(ExplorerCategoryButtonClicked);
-            ATPanel.Append(ExplorerCategoryButton);
 
-            Texture2D ChallengerCategoryTexture = ModContent.GetTexture("TerrariaAdvancements/UI/Images/Categories/ChallengerCategory");
-            ATHoverImageButton ChallengerCategoryButton = new ATHoverImageButton(ChallengerCategoryTexture, "Challenger Achievements");
             ChallengerCategoryButton.Width.Set(32, 0);
             ChallengerCategoryButton.Height.Set(32, 0);
             ChallengerCategoryButton.HAlign = 0.45f;
             ChallengerCategoryButton.VAlign = 0.005f;
             // ChallengerCategoryButton.OnClick += new MouseEvent(ChallengerCategoryButtonClicked);
+
+            TerrariaCategoryPanel.Width.Set(495, 0);
+            TerrariaCategoryPanel.Height.Set(230, 0);
+            TerrariaCategoryPanel.HAlign = 0.5f;
+            TerrariaCategoryPanel.VAlign = 0.95f;
+
+            TerrariaCategoryScrollbar.Height.Set(230, 0);
+            TerrariaCategoryScrollbar.SetView(100f, 200f);
+
+            TimberAchievement.Width.Set(64, 0);
+            TimberAchievement.Height.Set(64, 0);
+            TimberAchievement.HAlign = 0.1f;
+
+            Timber_HammerTime.Width.Set(100, 0);
+            Timber_HammerTime.Height.Set(10, 0);
+            Timber_HammerTime.Left.Pixels = 63;
+            Timber_HammerTime.VAlign = 0.5f;
+            Timber_HammerTime.BackgroundColor = Color.Black;
+
+            Append(ATPanel);
+
+            Append(HoverText);
+
+            ATPanel.Append(TerrariaCategoryButton);
+            ATPanel.Append(SlayerCategoryButton);
+            ATPanel.Append(CollectorCategoryButton);
+            ATPanel.Append(ExplorerCategoryButton);
             ATPanel.Append(ChallengerCategoryButton);
 
-            UIPanel SlayerCategoryPanel = new UIPanel();
-            SlayerCategoryPanel.Width.Set(495, 0);
-            SlayerCategoryPanel.Height.Set(230, 0);
-            SlayerCategoryPanel.HAlign = 0.5f;
-            SlayerCategoryPanel.VAlign = 0.95f;
-            ATPanel.Append(SlayerCategoryPanel);
+            ATPanel.Append(TerrariaCategoryPanel);
 
-            UIScrollbar ATScrollbar = new FixedUIScrollbar(ModContent.GetInstance<TerrariaAdvancements>().ATInterface);
-            ATScrollbar.Height.Set(230, 0);
-            ATScrollbar.SetView(100f, 200f);
-            SlayerCategoryPanel.Append(ATScrollbar);
+            TerrariaCategoryPanel.Append(TerrariaCategoryScrollbar);
 
-            ATHoverImage EyeOnYou_Achievement = new ATHoverImage(EyeOnYou_LockedTexture, "???");
-            EyeOnYou_Achievement.Width.Set(64, 0);
-            EyeOnYou_Achievement.Height.Set(64, 0);
-            EyeOnYou_Achievement.HAlign = 0.1f;
-
-            if (NPC.downedBoss1)
-            {
-                EyeOnYou_Achievement.SetImage(EyeOnYou_UnlockedTexture);
-                EyeOnYou_Achievement.HoverText = "Dingus";
-            }
-            else
-            {
-                EyeOnYou_Achievement.SetImage(EyeOnYou_LockedTexture);
-                EyeOnYou_Achievement.HoverText = "???";
-            }
-            SlayerCategoryPanel.Append(EyeOnYou_Achievement);
+            TerrariaCategoryPanel.Append(TimberAchievement);
+            TimberAchievement.Append(Timber_HammerTime);
         }
 
         public override void Update(GameTime gameTime)
         {
-            if (NPC.downedBoss1)
+            Player player = Main.LocalPlayer;
+
+            HoverText.Left.Pixels = Main.MouseScreen.X + 20f;
+            HoverText.Top.Pixels = Main.MouseScreen.Y + 20f;
+
+            if (player.GetModPlayer<TerrariaAdvancementsPlayer>().Timber != true)
             {
-                Main.NewText("Down");
-                // EyeOnYou_Achievement.SetImage(EyeOnYou_UnlockedTexture);
-                // EyeOnYou_Achievement.HoverText = "Dingus";
+                HoverText.SetText("???");
+                TimberAchievement.SetImage(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Achievements/Locked/Terraria/Timber_Locked"));
             }
             else
             {
-                Main.NewText("Not Down");
-                // EyeOnYou_Achievement.SetImage(EyeOnYou_LockedTexture);
-                // EyeOnYou_Achievement.HoverText = "???";
+                TimberAchievement.SetImage(ModContent.GetTexture("TerrariaAdvancements/UI/Images/Achievements/Unlocked/Terraria/Timber_Unlocked"));
+            }
+
+            if (TerrariaCategoryButton.IsMouseHovering)
+            {
+                HoverText.SetText("Terraria Achievements");
+            }
+            else if (SlayerCategoryButton.IsMouseHovering)
+            {
+                HoverText.SetText("Slayer Achievements");
+            }
+            else if (CollectorCategoryButton.IsMouseHovering)
+            {
+                HoverText.SetText("Collector Achievements");
+            }
+            else if (ExplorerCategoryButton.IsMouseHovering)
+            {
+                HoverText.SetText("Explorer Achievements");
+            }
+            else if (ChallengerCategoryButton.IsMouseHovering)
+            {
+                HoverText.SetText("Challenger Achievements");
+            }
+            else if (TimberAchievement.IsMouseHovering)
+            {
+                if (player.GetModPlayer<TerrariaAdvancementsPlayer>().Timber != true)
+                {
+                    HoverText.SetText("???");
+                }
+                else
+                {
+                    HoverText.SetText("[c/FFD700:Timber!!]\nChop down your first tree.");
+                }
+            }
+            else
+            {
+                HoverText.SetText("");
             }
         }
     }
